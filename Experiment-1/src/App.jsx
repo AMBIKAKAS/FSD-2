f7ebdd4c33712f4a4cabc90d0b882c3250e32aff
ambikakashyap736 <ambikakashyap736@gmail.com>
2026-01-16 10:53:05 +0530

    Add Experiment-1 as normal folder

---
+++ Experiment-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SinglePageApp from './component/Spa';
+import "./App.css"
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <SinglePageApp />
+      
+    </>
+  )
+}
+
+export default App
